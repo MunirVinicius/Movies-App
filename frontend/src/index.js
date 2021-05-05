4bd43b588139a18b390c85affb4056ea8f02b77c
MunirVinicius <munir.vinicius11@gmail.com>
2021-05-05 20:43:48 -0300

    Project Created

---
+++ frontend/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+class Main extends React.Component {
+  render(){
+    return (
+    <div className="container">
+      <h1 className="title">Movie Search</h1>
+    </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main/>, document.getElementById("root"));
